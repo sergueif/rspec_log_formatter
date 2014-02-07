a4e9d21f582e35cbbe5e04dfbf4cc44ec38ae1e8
Serguei Filimonov <sfilimonov@pivotallabs.com>
2014-02-07 14:18:00 -0800

    provide an option to truncate the file by builds

--- lib/rspec_log_formatter/analysis/analyzer.rb
+++ lib/rspec_log_formatter/analysis/analyzer.rb
@@ -6,14 +6,7 @@ module RspecLogFormatter
       def analyze(filepath, options = {})
         window = options[:last_builds]
 
-        build_numbers = []
-        results = File.open(filepath).each_line.map do |line|
-          result = Result.new(*CSV.parse_line(line, col_sep: "\t").first(7))
-          build_numbers << result.build_number
-          result
-        end
-        build_numbers.uniq!.sort!
-
+        build_numbers, results = result_numbers(filepath, options = {})
 
         scores = []
         results.group_by(&:description).each do |description, results|
@@ -33,6 +26,48 @@ module RspecLogFormatter
 
         scores.sort.map(&:as_hash)
       end
+
+      def truncate(filepath, opts = {})
+        builds = opts.fetch(:keep_builds)
+        build_numbers, results = result_numbers(filepath, options = {})
+        sio = StringIO.new
+
+        File.open(filepath, 'r').each_line do |line|
+          result = parse_line(line)
+          next unless build_numbers.last(builds).include? result.build_number
+          sio.puts line
+        end
+
+        sio.rewind
+        File.open(filepath, 'w') do |f|
+          f.write sio.read
+        end
+      end
+
+      private
+
+      def parse_line(line)
+        Result.new(*CSV.parse_line(line, col_sep: "\t").first(7))
+      end
+
+      def each_result(filepath, &block)
+        File.open(filepath, 'r').each_line do |line|
+          result = parse_line(line)
+          block.call(result)
+        end
+      end
+
+      def result_numbers(filepath, options = {})
+        build_numbers = []
+        results = []
+        each_result(filepath) do |result|
+          build_numbers << result.build_number
+          results << result
+        end
+        [build_numbers.uniq!.sort!, results]
+      end
+
     end
+
   end
 end
--- spec/lib/rspec_log_analyzer/analysis/analyzer_spec.rb
+++ spec/lib/rspec_log_analyzer/analysis/analyzer_spec.rb
@@ -1,4 +1,5 @@
 require "spec_helper"
+require 'tempfile'
 
 describe RspecLogFormatter::Analysis::Analyzer do
   it "sorts the parsed results by failure percentage" do
@@ -22,6 +23,19 @@ describe RspecLogFormatter::Analysis::Analyzer do
         "ec60\n      msg60",
       ]
     }
+  end
+
+
+  it "can truncate the log file" do
+    filepath = File.expand_path("../../../../fixtures/test_was_flaky_then_fixed.history", __FILE__)
+    temp = Tempfile.new('fixture')
+    FileUtils.copy(filepath, temp.path)
+    described_class.new.truncate(temp.path, keep_builds: 3)
+    File.open(temp.path, 'r').read.should == <<HEREDOC
+5	2014-01-21 16:08:25 -0800	passed	desc	./spec/m1k1_spec.rb
+6	2014-01-21 16:08:25 -0800	passed	desc	./spec/m1k1_spec.rb
+7	2014-01-21 16:08:25 -0800	passed	desc	./spec/m1k1_spec.rb
+HEREDOC
   end
 
   it "can analyze only a window of builds" do
