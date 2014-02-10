edfeebcd0c0457c8c92b368990bc9e1bb0aac2d3
Serguei Filimonov <pair+sfilimonov@pivotallabs.com>
2014-02-10 15:54:31 -0800

    add dummy app

---
+++ dummy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = '0a71576be2d8e6294861d77c3497cc02288526765b8ddfd8e43e5c27e197b7c32d299309ce110761738b3c64e8f63a7d42f2632a66276df1457a1f277e0fba53'
---
+++ dummy/spec/dummy_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe "Math" do
+  it "works" do
+    1.should == 1
+  end
+end
